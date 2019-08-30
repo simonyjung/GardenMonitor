e9b277b3df8a488faa38f2f74c38769d426d4f3c
Simon Jung <simon@trillfinancial.com>
2019-08-30 15:32:47 -0400

    add plant to admin

--- plants/models.py
+++ plants/models.py
@@ -7,3 +7,6 @@ class Plant(models.Model):
     name = models.CharField(max_length=255, blank=True)
     created_datetime = models.DateTimeField(default=datetime.datetime.now)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "<Plant {}>".format(self.name)
