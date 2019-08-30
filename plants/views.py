f57e422cab359fb457dabe733b0c142c7b43710d
Simon Jung <simon@trillfinancial.com>
2019-08-30 16:04:11 -0400

    add plant to admin

--- plants/views.py
+++ plants/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+from . import models
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    plants = models.Plant.objects.all()
+    context = {'plants': plants}
+    return render(request, 'index.html', context)
