3f66a9fb494302c6b083ef782d8fb0e37f25f9fd
simonyjung <jungk704@gmail.com>
2019-09-02 16:58:53 -0400

    play with analog to digital signal conversion

--- pi/test.py
+++ pi/test.py
@@ -1,36 +1,68 @@
-import RPi.GPIO as GPIO
+import smbus
+import time
 
-ledPin = 11  # define the ledPin
-buttonPin = 12  # define the buttonPin
+address = 0x48
+bus = smbus.SMBus(1)
+cmd = 0x40
+
+"""
+Reads value of analog soil moisture sensor and continously outputs reading
+"""
+
+MIN_HUMIDITY_VALUE = 220
+MAX_HUMIDITY_VALUE = 128
 
 
-def setup():
-    print ('Program is starting...')
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(ledPin, GPIO.OUT)
-    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def read_analog(channel):
+    """
+    Read ADC value
+    :param channel: 0, 1, 2, 3
+    :return:
+    """
+    value = bus.read_byte_data(address, cmd + channel)
+    return value
+
+
+def write_analog(value):
+    """
+    Write DAC value
+    :param value:
+    :return:
+    """
+    bus.write_byte_data(address, cmd, value)
+
+
+def get_humidity(value):
+    value_range = MIN_HUMIDITY_VALUE - MAX_HUMIDITY_VALUE
+    humidity_percentage = ((MIN_HUMIDITY_VALUE - value) / value_range) * 100
+    if humidity_percentage < 0:
+        return 0
+    elif humidity_percentage > 100:
+        return 100
+    else:
+        return round(humidity_percentage, 1)
 
 
 def loop():
     while True:
-        print(GPIO.input(buttonPin))
-        if GPIO.input(buttonPin) == GPIO.LOW:
-            GPIO.output(ledPin, GPIO.HIGH)
-            print ('led on ...')
-        else:
-            GPIO.output(ledPin, GPIO.LOW)
-            print ('led off ...')
+        # read the ADC value of channel 0
+        value = read_analog(0)
+        # write the DAC value
+        write_analog(value)
+        # calculate the voltage value
+        voltage = value / 255.0 * 3.3
+        soil_moisture = get_humidity(value)
+        print("Soil Moisture 1: {}%".format(soil_moisture))
+        time.sleep(0.5)
 
 
-# Set buttonPin's mode is
-def destroy():
-    GPIO.output(ledPin, GPIO.LOW)  # led off
-    GPIO.cleanup()  # Release resource
+def clean_up():
+    bus.close()
 
 
-if __name__ == '__main__': # Program start from here
-    setup()
+if __name__ == '__main__':
+    print("Program is starting...")
     try:
         loop()
-    except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the subprogram destroy() will be executed.
-        destroy()
+    except KeyboardInterrupt:
+        clean_up()
