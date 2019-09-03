b860a1413212e96f98fb0bdccd799217a9ceb3ab
simonyjung <jungk704@gmail.com>
2019-09-02 20:50:33 -0400

    add moisture level to dashboard

--- plants/models.py
+++ plants/models.py
@@ -10,9 +10,16 @@ class Plant(models.Model):
     def __str__(self):
         return "<Plant {}>".format(self.name)
 
+    @property
+    def latest_soil_moisture_measurement(self):
+        return self.soil_moisture_measurements.latest()
+
 
 class PlantSoilMoistureMeasurement(models.Model):
     plant = models.ForeignKey(Plant, related_name='soil_moisture_measurements', on_delete=models.SET_NULL,
                               null=True)
     created_datetime = models.DateTimeField(default=datetime.datetime.now)
     measurement = models.DecimalField(max_digits=4, decimal_places=1)
+
+    class Meta:
+        get_latest_by = 'created_datetime'
