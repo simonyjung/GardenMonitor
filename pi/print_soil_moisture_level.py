5d5eddf7950bc98d0c15347032f8eb54cf831ce8
simonyjung <jungk704@gmail.com>
2019-09-04 23:19:11 -0400

    play with analog to digital signal conversion

--- pi/print_soil_moisture_level.py
+++ pi/print_soil_moisture_level.py
@@ -102,8 +102,8 @@ def main(lcd_display=None):
         if lcd_display:
             lcd_message = ' '.join(["{} {}%".format(x['channel'], x['soil_moisture']) for x in soil_readings])
             if len(lcd_message) < 16:
-                lcd_message.ljust(16 - len(lcd_message))
-            lcd_display.message(lcd_message)
+                lcd_message = lcd_message.ljust(16 - len(lcd_message))
+            #lcd_display.message(lcd_message + )
         time.sleep(0.2)
 
 
